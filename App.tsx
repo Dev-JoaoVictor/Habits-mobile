d1ec391557bc2b87ae5f00c65ea2cc18da8c6ad7
João <joaovictor.ramos34@gmail.com>
2023-02-28 12:54:01 -0300

    importando a fonte Inter

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded) {
+    return;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World</Text>
@@ -13,11 +31,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#ffffff'
-  }
+    color: "#ffffff",
+  },
 });
