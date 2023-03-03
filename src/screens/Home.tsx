52f38468f48bb1bb193eb2cac36603350e3fbb9b
João <joaovictor.ramos34@gmail.com>
2023-03-03 11:02:56 -0300

    Adicionando key

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -26,8 +26,9 @@ export function Home() {
       </View>
 
       <ScrollView
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: 100}}>
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
             <HabitDay key={date.toISOString()} />
@@ -36,6 +37,7 @@ export function Home() {
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, index) => (
               <View
+                key={index}
                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
