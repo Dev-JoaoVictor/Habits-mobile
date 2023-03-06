242523e3eb2cc4b526d860586e149c0de546a0c1
João <joaovictor.ramos34@gmail.com>
2023-03-06 13:51:14 -0300

    Reaproveitando o checkbox

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -3,6 +3,7 @@ import { useRoute } from "@react-navigation/native";
 import { BackButton } from "../components/BackButton";
 import dayjs from "dayjs";
 import { ProgressBar } from "../components/ProgressBar";
+import { Checkbox } from "../components/Checkbox";
 
 interface Params {
   date: string;
@@ -26,6 +27,10 @@ export function Habit() {
         <Text className="mt-6 text-zinc-400 text-base font-semibold lowercase">{dayOfWeek}</Text>
         <Text className="text-white font-extrabold text-3xl">{dayAndMonth}</Text>
         <ProgressBar progress={75}/>
+        <View className="mt-6">
+          <Checkbox title="Beber 2L de água" checked={false}/>
+          <Checkbox title="Estudar programação" checked={true}/>
+        </View>
       </ScrollView>
     </View>
   );
