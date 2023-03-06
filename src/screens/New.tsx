ec08f4bf17669f6afdeaaa05a328cbfe1904e22b
João <joaovictor.ramos34@gmail.com>
2023-03-06 11:40:00 -0300

    Criando o input do formulário

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, Text, TextInput } from "react-native";
 
 import { BackButton } from "../components/BackButton";
 
@@ -6,7 +6,17 @@ export function New() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView showsVerticalScrollIndicator={false}>
-        <BackButton/>
+        <BackButton />
+        <Text className=" mt-6 text-3xl text-white font-extrabold">
+          Criar hábito
+        </Text>
+        <Text className=" mt-6 text-base font-semibold text-white">
+          Qual seu comprometimento?
+        </Text>
+        <TextInput 
+        placeholder="Exercícios, dormi bem, etc..."
+        className="bg-zinc-800 h-12 pl-4 rounded-lg mt-3 text-white focus:border-2 focus:border-green-600" 
+        />
       </ScrollView>
     </View>
   );
