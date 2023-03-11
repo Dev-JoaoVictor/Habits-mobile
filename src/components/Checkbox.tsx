aacb72749187705c4c303d20bf1845aeef5a9de3
João <joaovictor.ramos34@gmail.com>
2023-03-11 15:01:00 -0300

    Animando o checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,8 +1,14 @@
-import { TouchableOpacity, View, Text, TouchableOpacityProps } from "react-native";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  TouchableOpacityProps,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import Animated, {ZoomIn, ZoomOut} from "react-native-reanimated";
 
-interface Props extends TouchableOpacityProps{
+interface Props extends TouchableOpacityProps {
   title: string;
   checked?: boolean;
 }
@@ -15,15 +21,17 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
       {...rest}
     >
       {checked ? (
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+        <Animated.View 
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
           <Feather name="check" size={20} color={colors.white} />
-        </View>
+        </Animated.View>
       ) : (
-        <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
+        <View className="h-8 w-8 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
       )}
-      <Text className="text-white text-base ml-3 font-semibold">
-        {title}
-      </Text>
+      <Text className="text-white text-base ml-3 font-semibold">{title}</Text>
     </TouchableOpacity>
   );
 }
